10d94136c2075ed8f73b0993e25ba87305e5b6db
Austin.Lai <51234556+austin-lai@users.noreply.github.com>
2021-08-03 20:26:45 +0800

    Update python-http-connect.py

--- TryHackMe(THM)-HackBack 2019/python-http-connect.py
+++ TryHackMe(THM)-HackBack 2019/python-http-connect.py
@@ -6,55 +6,65 @@ from urllib import request
 
 
 HOST = '10.10.159.183'  # The server's hostname or IP address
-StartPORT = 3010       # The port used by the server
-LivePort = 1337        # This the port we want to get correct order of number
-int_port = 0
-Num = 0
-StartNum = 0
-TotalNum = 0
-second_true = ''
+StartPORT = 3010        # The port used by the server
+LivePort = 1337         # This the port we want to get correct order of number
+int_port = 0            # Variable to store the given port number from instruction as integer
+Num = 0                 # Variable to store dynamic value given from instruction
+StartNum = 0            # Variable and set to 0 from start to clear any value
+TotalNum = 0            # Variable to store value from calculation
+second_true = ''        # Counter check for second while loop
 
 
 try:
     # FIRST CONNECT with default given port 3010
     if (StartPORT == 3010):
         with request.urlopen('http://'+HOST+':'+str(StartPORT)+'/') as response:
+            
+            # Store the conetnt of http response into variable content
             content = response.read().decode('utf-8')
 
-            # Trim the response - 767 btyes anything before
+            # Trim 767 btyes off from the response - for the first http response
             http_trim = content[767:]
 
-            # Split the response into array with space
+            # Split and store the response into list type variable data_list with space
             data_list = list(http_trim.split(" "))
+            
+            # Check and print the content of list type variable data_list
             # print(data_list)
 
-            # Get the length of array
+            # Get the length of list type variable data_list
             # print("Length="+str(len(data_list)))
 
-            # Find the 30th index of array
+            # Find the 30th index of list
             index_30 = str(data_list[30])
-            # print(index_30, end = ' ') # Print without new line # Sample output of content # "http://"+window.location.hostname+":34232"</script>
+            
+            # Check and print the content inside list type variable index_30 --- without new line 
+            # Sample output of content
+            # "http://"+window.location.hostname+":34232"</script>
+            # print(index_30, end = ' ')  
 
-            # Split the data inside array
+            # Split the data inside the list type variable data_list
             # split_index_30 = data_list[30].split(':')
+            
+            # Check and print the content inside list type variable split_index_30 
             # print(split_index_30)
-
-            # if(port != StartPORT):
-
-            # Extract all number from the response
+            
+            # Extract all number from the response which currently store inside list type variable index_30
             port = re.findall(r'\d+', index_30)
 
             # Change to int
             int_port=int(port[0])
+            
+            # Check and confirm the type of variable int_port
             # print(type(int_port))
 
-            # print(index_30+str(int_port))
+            # Set value of int_port as StartPORT
             StartPORT=int_port
 
-    # print('Current Start Port = '+str(StartPORT))
     print('\nFirst request on port 3010 done ... Current port is set to = '+str(StartPORT))
 
-
+    
+    # Start first while loop that the StartPORT is not 9765
     while (StartPORT != 9765):
         try:
             if (StartPORT != 1337):
@@ -64,7 +74,10 @@ try:
 
                 with request.urlopen('http://'+HOST+':'+str(StartPORT)+'/') as response:
                     content = response.read().decode('utf-8')
-                    # print(content) # Sample output of content # multiply 1 6783
+                    
+                    # Sample output of content 
+                    # multiply 1 6783
+                    # print(content) 
 
                     data_list=content.split(" ")
 
@@ -89,7 +102,10 @@ try:
             with request.urlopen('http://'+HOST+':'+str(StartPORT)+'/') as response:
 
                 content = response.read().decode('utf-8')
-                # print(content) # Sample output of content # multiply 1 6783
+                
+                # Sample output of content 
+                # multiply 1 6783
+                # print(content) 
 
                 data_list=content.split(" ")
 
